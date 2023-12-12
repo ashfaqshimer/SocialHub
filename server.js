6e54e15f07fddd3ad76938663b5314f99d7ba24a
Ashfaq Shimer <shimer.ahamed@gmail.com>
2023-12-12 19:59:34 +0530

    Initialize server

---
+++ server.js
@@ -0,0 +1,67 @@
+// const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+require('dotenv').config();
+const colors = require('colors');
+const cors = require('cors');
+
+// Local imports
+// const connectDb = require('./config/db');
+// const colors = require('colors');
+// const errorHandler = require('./middleware/errorHandler');
+
+// Connect to database
+// connectDb();
+
+// Route files
+// const bootcamps = require('./routes/bootcamps');
+// const courses = require('./routes/courses');
+// const auth = require('./routes/auth');
+// const users = require('./routes/users');
+// const reviews = require('./routes/reviews');
+
+const app = express();
+
+// Body parser~~
+app.use(express.json());
+
+// Dev loggin middleware
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
+}
+
+// Rate limiting
+// const limiter = rateLimit({ windowMs: 10 * 60 * 1000, max: 100 }); // 10 mins
+// app.use(limiter);
+
+// Enable CORS
+app.use(cors());
+
+// // Set static folder
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// Mount routers
+// app.use('/api/v1/bootcamps', bootcamps);
+// app.use('/api/v1/courses', courses);
+// app.use('/api/v1/auth', auth);
+// app.use('/api/v1/users', users);
+// app.use('/api/v1/reviews', reviews);
+
+// Error handler
+// app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+	PORT,
+	console.log(
+		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+	)
+);
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+	console.error(`Error: ${err.message}`.red);
+	// Close server and exit process
+	server.close(() => process.exit(1));
+});
